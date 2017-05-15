6b6a8f7ac738fd9f8b72e30481ab7d5eff6f66e6
Maks <andreev171293@gmail.com>
2017-05-15 19:08:27 +0300

    Add Profile Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import React, {Component} from 'react';
+import {FormGroup, FormControl,InputGroup,Glyphicon} from 'react-bootstrap';
+import Profile from './Profile';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      searchText: '',
+      artist: null
+    };
+    this.search = this.search.bind(this);
+  }
+  search(){
+    const ROOT_URL = 'https://api.spotify.com/v1/search';
+    const FETCH_URL = `${ROOT_URL}?q=${this.state.searchText}&type=artist&limit=1`;
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];
+      this.setState({artist});
+      console.log(this.state.artist);
+    })
+
+  }
   render(){
-    return(<div>
-      Music Master
-    </div>)
+    return(
+      <div className='app'>
+        <h3 className='title'>Music Master</h3>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              value={this.state.searchText}
+              placeholder='Search an artist...'
+              onChange={event => this.setState({searchText: event.target.value})}
+              onKeyPress={(event) => {
+                if(event.key === "Enter"){
+                  this.search()
+                }
+              }}
+            />
+            <InputGroup.Addon
+              onClick={this.search}
+              >
+              <Glyphicon glyph='search' />
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+
+        <Profile
+          artist={this.state.artist}
+        />
+        <div className='gallery'>
+          Gallery
+        </div>
+      </div>)
   }
 }
 
---
+++ src/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+class Profile extends React.Component{
+  render(){
+    return(
+      <div>
+        <h3>Profile Name</h3>
+        <p>Profile followers</p>
+      </div>
+    )
+  }
+}
+
+export default Profile;
